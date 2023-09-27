c901e5b74627785b36201b523dc3c87cbb8516cd
izmelucifer17 <108258417+izmelucifer17@users.noreply.github.com>
2023-09-27 23:44:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ import sys
 import shutil
 import numpy as np
 import time
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from scipy.stats import randint
 
 
 pickle_in = open("classifier.pkl","rb")
